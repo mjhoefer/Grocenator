2126afd73441a5f797983d31d33c950b8a608870
Michael Hoefer <mjhoefer@gmail.com>
2015-09-19 11:51:53 -0500

    added initial python json script

--- Keyboard IO/test1.py
+++ Keyboard IO/test1.py
@@ -1,12 +1,46 @@
 import csv
+import json
+
+print("Your options are: \n")
+with open('itemList.csv', 'rb') as csvfile:
+    itemListReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in itemListReader:
+        print ', '.join(row)
 
 item = "hey"
 print("Thank you for using PMS, ")
 item = raw_input("order something:")
 
-print"you ordered item", item
+print "you ordered item", item
 
-with open('eggs.csv', 'rb') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-    print ', '.join(row)
+
+
+jimmyOrder = {
+  "email": "",
+  "company": "AllPlayers.com",
+  "address": "14665 Midway Road",
+  "apt/suite": "220",
+  "city": "Addison",
+  "state": "Texas",
+  "zip": "75001",
+  "sandwich": "country club",
+  "who": "Travis",
+  "bread": "French Bread",
+  "cut": "true",
+  "drink": "",
+  "chips": "Regular Jimmy Chips",
+  "cookie": "",
+  "pickle": "",
+  "Tomato": "NO",
+  "tip": "2",
+  "billing_address": "123 Main St.",
+  "billing_city": "Carrollton",
+  "billing_state": "Texas",
+  "billing_zip": "75007"
+}
+
+print(json.dumps(jimmyOrder))
+
+f = open('order.json', 'w')
+f.write(json.dumps(jimmyOrder))
+f.close()
